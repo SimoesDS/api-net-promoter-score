261810b00458acaf516fc8254776c54c7b279464
SimoesDS <simoes.sdgo@gmail.com>
2021-03-04 00:29:59 -0300

    configuring connection data to the database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { Request, Response} from 'express';
+import './database';
 
 const app = express();
+
+app.get('/', (request: Request, response: Response) => {
+    return response.json({
+        message: 'Hello World!',
+    });
+});
+
+app.post('/', (request: Request, response: Response) => {
+    return response.json({
+        message: 'Os dados foram salvos com sucesso!',
+    });
+});
 
 app.listen(3333, () => console.log('Server is running'));
